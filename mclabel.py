9a36de697c6a8c9ea1cd15b7ec5344784531c7b2
Jonas Utz <jonas.utz@fau.de>
2022-11-25 13:40:06 +0000

    Make package

--- mclabel.py
+++ mclabel.py
@@ -1,60 +1,41 @@
 import numpy as np
 import napari
-import tifffile
 from scipy import ndimage
-from skimage import data
-import matplotlib.pyplot as plt
-from qtpy.QtWidgets import QPushButton, QMainWindow, QCheckBox, QSpinBox, QLabel, QSlider, QShortcut
+from qtpy.QtWidgets import QPushButton, QMainWindow, QCheckBox, QSpinBox, QLabel, QSlider, QShortcut, QWidget
 from qtpy.QtCore import *
 from qtpy.QtGui import QKeySequence
 import skimage
-from skimage.segmentation import clear_border
 from skimage.measure import label, regionprops, regionprops_table
-from skimage.morphology import closing, square
 from utils import fill_label_holes, fill_label_holes_cv2
 from skimage.util import map_array
 from enum import Enum
+from napari_plugin_engine import napari_hook_implementation
+
 
 class State(Enum):
-    IDLE = 1
-    DRAW = 2
-    COMPUTE = 3
+    DRAW = 1
+    COMPUTE = 2
+    NO_INIT = 3
 
-class McLabel(QMainWindow):
+
+class McLabel(QWidget):
 
     def __init__(self, napari_viewer):
         super().__init__()
         self.viewer = napari_viewer
-        self.image_layer = self.viewer.layers['macrophages']
-        self.label_layer = self.viewer.add_labels(np.zeros_like(self.image_layer.data, dtype='int32'),
-                                                  name='OutputLabel')
+        self.viewer.layers.events.inserted.connect(self.init_layers)
+
         self.label_helper = None
+        self.image_layer = None
+        self.label_layer = None
+
         # start_button
-        self.start_button = QPushButton('Draw new Label')
-        self.start_button.clicked.connect(self.select_macrophage)
-        # self.viewer.window.add_dock_widget(self.start_button, area='left')
-        # finish buttono
-        self.finish_button = QPushButton('Compute Label')
-        self.finish_button.clicked.connect(self.finish_fn)
+        self.start_button = QPushButton('Draw Label')
+        self.start_button.clicked.connect(self.btn_click)
 
-
-
-        self.compute_label_shortcut = QShortcut(QKeySequence(Qt.CTRL + Qt.Key_P), self)
-        self.compute_label_shortcut.activated.connect(self.finish_fn)
-
-
-        # Checkbox for preserve labels
-        self.preserve_checkbox = QCheckBox("Preserve Label")
-
-        # Slider for area which will be discarded
-        self.area_slider_lbl = QLabel('Min Area: ')
-        self.area_slider = QSlider(Qt.Orientation.Horizontal)
-        self.area_slider.setRange(250, 250000)
-        self.area_slider.setValue(100)
-        self.area_slider.setPageStep(250)
-        self.area_slider.setVisible(False)
-        self.area_slider_lbl.setVisible(False)
-        self.area_slider.valueChanged.connect(self.manual_area_adjustment)
+        # preprocess button
+        self.preproc_button = QPushButton('Convert Images')
+        self.preproc_button.clicked.connect(self.preproc_fn)
 
         # Slider for manual adjustment of threshold
         self.threshold_slider_lbl = QLabel("Threshold: ")
@@ -67,30 +48,88 @@ class McLabel(QMainWindow):
         self.threshold_slider.valueChanged.connect(self.manual_threshold_adjustment)
 
         self.viewer.window.add_dock_widget(
-            [self.start_button, self.finish_button, self.preserve_checkbox, self.area_slider_lbl, self.area_slider,
-             self.threshold_slider_lbl, self.threshold_slider], area='left', name='McLabel')
+            [self.start_button, self.preproc_button, self.threshold_slider_lbl, self.threshold_slider], area='left',
+            name='McLabel')
 
         self.current_max_lbl = 0
-        self.finish_button.setEnabled(False)
-        self.select_macrophage()
-        # TODO:
-        """
-        Implement global flags for draw mode. If draw mode is active, the button changes into an undo button 
-        which deletes the label helper if pressed and retains the original state
-        
-        save bbox coordinates in dict for later adaption
-        """
+        self.start_button.setEnabled(False)
 
-    def select_macrophage(self):
-        self.finish_button.setEnabled(True)
+        self.state = State.NO_INIT
+
+        self.normalize = False
+
+    def init_layers(self, event):
+        if len(self.viewer.layers) == 1:
+            # If an image was added we can now allow labelling (i.e. enable the button)
+            self.start_button.setEnabled(True)
+
+    def preproc_fn(self):
+        if isinstance(self.viewer.layers[0].data, napari.layers._multiscale_data.MultiScaleData):
+            # user manually selects all layers which shall be considered for MIP
+            num_layers_selected = len(self.viewer.layers.selection)
+            assert num_layers_selected > 0, "At least one layer must be selected"
+
+            singleton = self.viewer.layers[0].data.shape[
+                            0] == 1  # sometimes the ims loader returns the array with singleton
+
+            if singleton:  # maybe we do not need this
+                _, z, y, x = self.viewer.layers[0].data[0].shape
+            else:
+                z, y, x = self.viewer.layers[0].data[0].shape
+
+            max_intensity_img = np.zeros((2, y, x))
+            # TODO:
+            # merge max value from all selected layers into one channel
+            # channels that are not selected will remain as distinct channels
+            for selected in self.viewer.layers.selection:
+                sel = np.asarray(selected.data[0])[0:self.viewer.layers[0].data[0].shape[0]]
+                current_mip = np.amax(sel, axis=0)
+                np.copyto(max_intensity_img[0], current_mip, where=current_mip > max_intensity_img[0])
+
+            probably_nuc_channel = np.asarray(
+                [l for l in self.viewer.layers if l not in self.viewer.layers.selection][0].data[0].copy())[
+                                   0:self.viewer.layers[0].data[0].shape[0]]
+            max_intensity_img[1] = np.amax(probably_nuc_channel, axis=0)
+
+        else:  # no ims file, image presumed to be already processed
+            max_intensity_img = self.viewer.layers[0].data.copy()
+
+        self.viewer.layers.clear()
+        self.viewer.add_image(max_intensity_img,
+                              channel_axis=0,
+                              name=["macrophages", "nuclei"],
+                              colormap=["gray", "magenta"],
+                              contrast_limits=[[0, 75], [0, 75]],
+                              )
+        self.image_layer = self.viewer.layers['macrophages']
+        self.label_layer = self.viewer.add_labels(np.zeros_like(self.image_layer.data, dtype='int32'),
+                                                  name='OutputLabel')
+
+        self.viewer.reset_view()
+        self.preproc_button.setVisible(False)
+        self.start_button.setText("Compute Label")
+        self.draw_fn()
+
+    def btn_click(self):
+        if self.state == State.DRAW:
+            self.start_button.setText("Draw Label")
+            self.compute_fn()
+        elif self.state == State.COMPUTE:
+            self.start_button.setText("Compute Label")
+            self.draw_fn()
+
+    def draw_fn(self):
+        self.state = State.DRAW
         labels = np.zeros_like(self.image_layer.data, dtype='int32')
         self.label_helper = self.viewer.add_labels(labels, name='label_helper')
+        self.viewer.layers[0].selected = False
+        self.viewer.layers['label_helper'].selected = True
+
         self.label_helper.mode = "PAINT"
         # print("Hello Not-Foo!")
 
-    def finish_fn(self):
-        # TODO: implement error handling
-
+    def compute_fn(self):
+        self.state = State.COMPUTE
         # Get data
         img_patch, (minr, minc, maxr, maxc) = self.get_patch_from_layer()
 
@@ -112,8 +151,7 @@ class McLabel(QMainWindow):
 
         self.threshold_slider_lbl.setVisible(True)
         self.threshold_slider.setVisible(True)
-        self.area_slider_lbl.setVisible(True)
-        self.area_slider.setVisible(True)
+
         print("Done")
 
     @staticmethod
@@ -158,7 +196,7 @@ class McLabel(QMainWindow):
             print(f'{minr=},{minc=},{maxr=},{maxc=}')
             print(prop.area)
         img_patch = self.image_layer.data[minr:maxr, minc:maxc].copy()
-        img_patch[labeled_macro[minr:maxr, minc:maxc] == 0] = 0 # removes parts outside of handdrawn region.
+        img_patch[labeled_macro[minr:maxr, minc:maxc] == 0] = 0  # removes parts outside of handdrawn region.
 
         return img_patch, (minr, minc, maxr, maxc)
 
@@ -190,33 +228,15 @@ class McLabel(QMainWindow):
         self.label_layer.refresh()
         print(f'DEBUG: {thresh=}')
 
-    def manual_area_adjustment(self, area):
-        img_patch, (minr, minc, maxr, maxc) = self.get_patch_from_layer()
-        lbl_patch = self.label_layer.data[minr:maxr, minc:maxc]
-        filtered_label = self.apply_filter(lbl_patch, min_value=area)
-        label_patch = self.label_layer.data[minr:maxr, minc:maxc]
-        out_patch_1 = np.where(label_patch == 0, filtered_label, label_patch)
-        out_patch_2 = np.where(label_patch == self.current_max_lbl, filtered_label, label_patch)
-        out_patch = out_patch_1 + out_patch_2
-        self.label_layer.data[minr:maxr, minc:maxc] = out_patch
-        self.label_layer.refresh()
-        print(f'DEBUG: {area=}')
 
-
+@napari_hook_implementation
+def napari_experimental_provide_dock_widget():
+    return McLabel
 
 def main():
     # Load sample image
-    img = tifffile.imread(
-        "/Users/jonas/Library/Mobile Documents/com~apple~CloudDocs/FAU/AIMI/PhD/03_Datasets/ouma_macrophages/TIF/CD115_l_Ctrl _Merged Image 12.tiff")
-
-    viewer = napari.view_image(
-        img,
-        channel_axis=0,
-        name=["macrophages", "nuclei"],
-        colormap=["gray", "magenta"],
-        contrast_limits=[[0, 75], [0, 75]],
-    )
-    _ = McLabel(viewer)
+    viewer = napari.Viewer()
+    win = McLabel(viewer)
     input('Press ENTER to exit')
 
 
