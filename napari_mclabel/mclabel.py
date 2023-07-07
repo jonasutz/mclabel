04cdea71cfc1651d54110a6c8dabb6916e067f33
Jonas Utz <jonas.utz@fau.de>
2023-07-07 13:54:37 +0200

    removed timer which was used for paper

--- napari_mclabel/mclabel.py
+++ napari_mclabel/mclabel.py
@@ -16,9 +16,10 @@ from napari_plugin_engine import napari_hook_implementation
 import imageio
 import csv
 import os
-from segment_anything import SamPredictor, sam_model_registry # Schau ma mal, was' wird
+from segment_anything import SamPredictor, sam_model_registry  # Schau ma mal, was' wird
 from segment_anything.automatic_mask_generator import SamAutomaticMaskGenerator
 import torch
+
 
 class State(Enum):
     DRAW = 1
@@ -51,19 +52,9 @@ class McLabel(QWidget):
 
         self.layer_types = {"image": napari.layers.image.image.Image, "labels": napari.layers.labels.labels.Labels}
 
-        # start_button
+        # draw_button
         self.start_button = QPushButton('Draw Label')
         self.start_button.clicked.connect(self.btn_click)
-
-        # Timer for user study
-        self.timer_start_stop = QPushButton('Start Timer')
-        self.timer_start_stop.clicked.connect(self.timer_fn)
-
-        # Checkbox for user study
-        self.manual_mode = False
-        self.manual_mode_cb = QCheckBox("Manual Annotation")
-        self.manual_mode_cb.setChecked(self.manual_mode)
-        self.manual_mode_cb.toggled.connect(self.change_manual_mode)
 
         # List of layers to (select layer to segment)
         self.layer_selection_lbl = QLabel("Select layer to segment:")
@@ -93,14 +84,15 @@ class McLabel(QWidget):
         self.threshold_slider = QSlider(Qt.Orientation.Horizontal)
         self.threshold_slider.setVisible(False)
         self.threshold_slider_lbl.setVisible(False)
-        self.threshold_slider.setRange(0, 65000) # TODO: determine range from selected image
+        self.threshold_slider.setRange(0, 65000)  # TODO: determine range from selected image
         self.threshold_slider.setPageStep(1)
         self.threshold_slider.setValue(15)
         self.threshold_slider.valueChanged.connect(self.manual_threshold_adjustment)
 
         self.viewer.window.add_dock_widget(
-            [self.manual_mode_cb, self.start_button, self.preproc_button, self.timer_start_stop,
-             self.layer_selection_lbl, self.layer_selection_cb, self.algo_selection_lbl,self.algo_selection_cb,self.threshold_slider_lbl, self.threshold_slider],
+            [self.start_button, self.preproc_button,
+             self.layer_selection_lbl, self.layer_selection_cb, self.algo_selection_lbl, self.algo_selection_cb,
+             self.threshold_slider_lbl, self.threshold_slider],
             area='right',
             name='McLabel')
 
@@ -110,16 +102,6 @@ class McLabel(QWidget):
         self.state = State.NO_INIT
 
         self.normalize = False
-
-        self.timer_active = False
-        self.start_time = 0
-        self.end_time = 0
-        self.duration = 0
-        self.logfolder = McLabel.create_logfolder()
-        self.filename = os.path.join(self.logfolder, 'log.csv')
-
-        self.logline = "label, minr, minc, maxc, duration"
-        self.write_logline(self.logline)
 
         # Stuff for sam
         if not torch.cuda.is_available():
@@ -135,18 +117,8 @@ class McLabel(QWidget):
         self.sam_logits = None
         self.sam_features = None
 
-        self._load_model() #TODO: Load sam only when required
+        self._load_model()  # TODO: Load sam only when required
 
-
-    def change_manual_mode(self):
-        self.manual_mode = self.manual_mode_cb.isChecked()
-        print(f'Set manual mode to {self.manual_mode}')
-        if self.label_helper is not None:
-            self.viewer.layers.remove(self.label_helper)
-            self.label_helper = None
-            self.viewer.layers.selection.active = self.viewer.layers["OutputLabel"]
-            self.start_button.setEnabled(False)
-            self.label_layer.brush_size = 3
 
     @staticmethod
     def create_logfolder():
@@ -164,28 +136,6 @@ class McLabel(QWidget):
             # If an image was added we can now allow labelling (i.e. enable the button)
             self.start_button.setEnabled(True)
 
-    def write_logline(self, logline):
-        with open(self.filename, 'a', newline='') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
-            writer.writerow([logline])
-        self.logline = ""
-
-    def timer_fn(self):
-        if self.timer_active:
-            self.end_time = time.time()
-            self.duration = round(time.time() - self.start_time)
-            print(f'It took {self.duration} seconds')
-            if self.manual_mode:
-                self.logline += str(f'{self.label_layer.selected_label},')
-            self.logline += str(self.duration)
-            print(self.logline)
-            self.write_logline(self.logline)
-            self.timer_active = False
-            self.timer_start_stop.setText("Start Timer")
-        else:
-            self.start_time = time.time()
-            self.timer_active = True
-            self.timer_start_stop.setText("Stop Timer")
 
     def preproc_fn(self):
         if isinstance(self.viewer.layers[0].data, napari.layers._multiscale_data.MultiScaleData):
@@ -237,7 +187,7 @@ class McLabel(QWidget):
         self.label_layer.events.selected_label.connect(on_label_change)
         # self.label_layer.selected_label = 0
         self.viewer.reset_view()
-        self.threshold_slider.setRange(0, self.image_layer.data.max() // 2) # TODO: Move this to somewhere else.
+        self.threshold_slider.setRange(0, self.image_layer.data.max() // 2)  # TODO: Move this to somewhere else.
         self.preproc_button.setVisible(False)
         self.start_button.setText("Compute Label")
         self.draw_fn()
@@ -252,7 +202,8 @@ class McLabel(QWidget):
 
     def draw_fn(self):
         self.state = State.DRAW
-        labels = np.zeros(self.image_layer.data.shape[self.viewer.dims.ndim - self.viewer.dims.ndisplay:], dtype='int32') # label helper is always 2D
+        labels = np.zeros(self.image_layer.data.shape[self.viewer.dims.ndim - self.viewer.dims.ndisplay:],
+                          dtype='int32')  # label helper is always 2D
         self.label_helper = self.viewer.add_labels(labels, name='label_helper')
         self.label_helper.brush_size = 17
         self.viewer.layers[0].selected = False
@@ -266,7 +217,7 @@ class McLabel(QWidget):
         img_patch, (minr, minc, maxr, maxc) = self.get_patch_from_layer()
 
         # Apply image processing
-        #filtered_label = self.compute_label_from_patch(img_patch).astype('int32')
+        # filtered_label = self.compute_label_from_patch(img_patch).astype('int32')
         if self.algo == "SAM":
             filtered_label = self.compute_label_from_patch_sam_prompt(img_patch).astype('int32')
         else:
@@ -274,8 +225,6 @@ class McLabel(QWidget):
 
         # Adapt counting of filtered label to current_max_lbl + 1
         self.current_max_lbl += 1
-        self.logline += str(self.current_max_lbl) + ","
-        self.logline += f'{minr}, {minc}, {maxr}, {maxc},'
         filtered_label[filtered_label != 0] = self.current_max_lbl
 
         # Refresh layers
@@ -326,23 +275,22 @@ class McLabel(QWidget):
         self.points = borderpoints[np.random.choice(borderpoints.shape[0], 20, replace=False), :]
         labeled_macro = ndimage.binary_fill_holes(labeled_macro).astype('int32')
         props = regionprops(labeled_macro)
-        print(f'{len(props)=}')
+        
         for prop in props:
             minr, minc, maxr, maxc = prop.bbox
         img_patch = self.image_layer.data[minr:maxr,
                     minc:maxc].copy() if self.viewer.dims.ndim == 2 else self.image_layer.data[
-                                                                         self.viewer.dims.current_step[0], # z
-                                                                         minr:maxr, # y
-                                                                         minc:maxc].copy() # x
+                                                                         self.viewer.dims.current_step[0],  # z
+                                                                         minr:maxr,  # y
+                                                                         minc:maxc].copy()  # x
         img_patch[
             labeled_macro[minr:maxr, minc:maxc] == 0] = 0  # removes parts outside hand-drawn region. TODO: 3D handling
 
-        # imageio.imsave(os.path.join(self.logfolder, f'{self.current_max_lbl}.png'), img_patch)
         return img_patch, (minr, minc, maxr, maxc)
 
     def compute_label_from_patch(self, img_patch, thresh=None, min_area=None):
         if thresh is None:
-            #thresh = skimage.filters.threshold_triangle(img_patch, nbins=32)
+            # thresh = skimage.filters.threshold_triangle(img_patch, nbins=32)
             thresh = self.algo(img_patch, nbins=32)
         binary = McLabel.apply_threshold(img_patch, thresh)
         label_image = McLabel.connected_component(binary)
@@ -355,7 +303,7 @@ class McLabel(QWidget):
     def manual_threshold_adjustment(self, thresh):
         img_patch, (minr, minc, maxr, maxc) = self.get_patch_from_layer()
         filtered_label = self.compute_label_from_patch(img_patch, thresh=thresh)
-        label_patch = self.label_layer.data[self.viewer.dims.current_step[0],minr:maxr, minc:maxc].copy()
+        label_patch = self.label_layer.data[self.viewer.dims.current_step[0], minr:maxr, minc:maxc].copy()
         # make sure that we keep original label id when changing threshold
         if label_patch.max():
             # filtered_label[filtered_label != 0] = label_patch.max()
@@ -364,9 +312,8 @@ class McLabel(QWidget):
         out_patch = label_patch.copy()
         np.copyto(out_patch, filtered_label, where=label_patch == 0)
         np.copyto(out_patch, filtered_label, where=label_patch == self.current_max_lbl)
-        self.label_layer.data[self.viewer.dims.current_step[0],minr:maxr, minc:maxc] = out_patch
+        self.label_layer.data[self.viewer.dims.current_step[0], minr:maxr, minc:maxc] = out_patch
         self.label_layer.refresh()
-
 
     def on_image_change(self):
         # self.image_layer = self.viewer.layers[self.layer_selection_cb.currentText()]
@@ -447,7 +394,8 @@ class McLabel(QWidget):
         self.sam_model = sam_model_registry['vit_h']("/Users/jonas/.cache/napari-segment-anything/sam_vit_h_4b8939.pth")
         self.sam_model.to(self.device)
         self.sam_predictor = SamPredictor(self.sam_model)
-        self.sam_anything_predictor = SamAutomaticMaskGenerator(model=self.sam_model, pred_iou_thresh=0.75, stability_score_thresh=0.75)
+        self.sam_anything_predictor = SamAutomaticMaskGenerator(model=self.sam_model, pred_iou_thresh=0.75,
+                                                                stability_score_thresh=0.75)
 
     def compute_label_from_patch_sam(self, image_patch):
         # TODO: make sure we have uint8_t
@@ -482,9 +430,9 @@ class McLabel(QWidget):
             return_logits=False,
         )
         prediction = prediction[0]
-        #prediction = np.abs(prediction)
-        #prediction = np.round(prediction).astype('int32')
-        #prediction[prediction > 0] = 1
+        # prediction = np.abs(prediction)
+        # prediction = np.round(prediction).astype('int32')
+        # prediction[prediction > 0] = 1
         # wenn dat so funktioniert wärs ja megaaa
         return prediction
 
